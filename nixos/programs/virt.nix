a7c34f251c0264b00f29a96f3ba22507fb0ea19d
Locbac <martinhost003@gmail.com>
2024-03-10 09:54:37 -0400

    push

--- nixos/programs/virt.nix
+++ nixos/programs/virt.nix
@@ -18,9 +18,9 @@
   };
   programs.virt-manager.enable = true;
   dconf.settings = {
-  "org/virt-manager/virt-manager/connections" = {
-    autoconnect = ["qemu:///system"];
-    uris = ["qemu:///system"];
+    "org/virt-manager/virt-manager/connections" = {
+      autoconnect = ["qemu:///system"];
+      uris = ["qemu:///system"];
     };
   };
   environment = {
