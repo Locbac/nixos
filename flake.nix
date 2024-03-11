2c43de25593079e5efa62b579c58aff683a14b16
Locbac <martinhost003@gmail.com>
2024-03-11 10:06:12 -0400

    push

--- flake.nix
+++ flake.nix
@@ -74,8 +74,8 @@
           # > Our main nixos configuration file <
           ./nixos/configuration.nix
           nixos-hardware.nixosModules.dell-precision-5530
-          #nixos-hardware.nixosModules.common-gpu-nvidia-disable #Disable Nvidia
-          nixos-hardware.nixosModules.common-gpu-nvidia-prime #NVIDIA Prime
+          nixos-hardware.nixosModules.common-gpu-nvidia-disable #Disable Nvidia
+          #nixos-hardware.nixosModules.common-gpu-nvidia-prime #NVIDIA Prime
           #nixos-hardware.nixosModules.common-gpu-nvidia-default #NVIDIA Defaults
         ];
       };
