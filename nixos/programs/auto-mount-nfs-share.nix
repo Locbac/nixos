11888549f7f82d44b4b05683a5270501a815cbd7
Locbac <martinhost003@gmail.com>
2024-03-12 10:02:15 -0400

    push

--- nixos/programs/auto-mount-nfs-share.nix
+++ nixos/programs/auto-mount-nfs-share.nix
@@ -1,6 +1,7 @@
 { inputs, outputs, lib, config, pkgs, ... }:
 {
-  fileSystems./home/amon/windows-nfs/ = {
+  fileSystems."/home/amon/windows-nfs/" = 
+  {
     device = "192.168.0.101:/OldNerd";
     fsType = "nfs";
     options = [ "x-systemd.automount" "x-systemd.idle-timeout=600" "noauto" ];
