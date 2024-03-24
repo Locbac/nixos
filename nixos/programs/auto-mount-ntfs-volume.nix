f225766387df70201e7f9eb91b05ccfeafd486a1
Locbac <martinhost003@gmail.com>
2024-03-24 08:28:16 -0400

    push

--- nixos/programs/auto-mount-ntfs-volume.nix
+++ nixos/programs/auto-mount-ntfs-volume.nix
@@ -1,7 +1,7 @@
 { inputs, outputs, lib, config, pkgs, ... }:
 {
   ## AUTO-MOUNT NTFS STORAGE
-  fileSystems."/media/amon/Storage" =
+  fileSystems."/run/media/amon/Storage" =
     { device = "/dev/disk/by-uuid/E6CCD727CCD6F0B3";
       fsType = "ntfs-3g"; 
       options = [ "rw" "uid=1000"];
