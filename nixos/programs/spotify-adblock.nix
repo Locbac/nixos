2c6011f098df082037d197fb754888e0e5249de2
Locbac <martinhost003@gmail.com>
2024-03-27 18:26:13 -0400

    push

--- nixos/programs/spotify-adblock.nix
+++ nixos/programs/spotify-adblock.nix
@@ -1,7 +1,12 @@
 { config, pkgs, inputs, ... }:
 {
   nixpkgs.config.packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+    nur = import (builtins.fetchTarball { 
+      url = "https://github.com/nix-community/NUR/archive/master.tar.gz" ;
+      sha256 = "1lrfpiybk6aii77zskfl5q2hci79nvx0x2a31rjlcqzpfmp0m663";
+        }
+      ) 
+      {
       inherit pkgs;
     };
   };
