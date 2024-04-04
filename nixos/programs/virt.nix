da6e4ac18a679a880ba8db0b01f552fe54c458ea
Locbac <martinhost003@gmail.com>
2024-04-04 09:14:32 -0400

    push

--- nixos/programs/virt.nix
+++ nixos/programs/virt.nix
@@ -41,4 +41,16 @@
     users.amon.extraGroups = [ "libvirtd" "virt-manager" ];
     groups.libvirtd.members = [ "root" "amon" ];
   };
+
+# QEMU.CONF FILE DOWN BELOW
+  virtualisation.libvirtd.qemu.verbatimConfig = ''
+  cgroup_device_acl = [
+          "/dev/null", "/dev/full", "/dev/zero", 
+          "/dev/random", "/dev/urandom",
+          "/dev/ptmx", "/dev/kvm", "/dev/kqemu",
+          "/dev/rtc","/dev/hpet",
+          "/dev/input/by-id/KEYBOARD_NAME",
+          "/dev/input/by-id/MOUSE_NAME"
+  ]
+  '';
 }
